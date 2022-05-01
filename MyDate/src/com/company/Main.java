9e1ae0b7db100560d6f9c009696776ab7c12c21b
innysik <innysik@hotmail.com>
2022-05-01 13:25:54 +0200

    new class mydatelist added

--- MyDate/src/com/company/Main.java
+++ MyDate/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +9,9 @@ public class Main {
         MyDate date1 = new MyDate("2021-02-23");
         System.out.println(date1);
         MyDate date2 = new MyDate(25,3);
-        System.out.println(date2.compareTo(date1));
-        System.out.println(date1.getDifferenceBetweenDays(date2));
-        System.out.println(MyDate.getDifferenceBetweenDays(date1,date2));
+        //System.out.println(date2.compareTo(date1));
+        MyDate date3 = new MyDate(8,1,1995);
+
+
     }
 }
--- MyDate/src/com/company/MyDate.java
+++ MyDate/src/com/company/MyDate.java
@@ -1,15 +1,15 @@
 package com.company;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.chrono.ChronoLocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
+import java.util.Arrays;
+
 
 public class MyDate implements Comparable<MyDate> {
     private int day;
     private int month;
     private int year;
+
 
 
     public MyDate(int day, int month, int year) {
@@ -24,12 +24,15 @@ public class MyDate implements Comparable<MyDate> {
         this.year = 2022;
     }
 
+
+
     public MyDate(String date) {
-        String[] parseDate= date.split("-");
+        String[] parseDate = date.split("-");
         this.year = Integer.parseInt(parseDate[0]);
         this.month = Integer.parseInt(parseDate[1]);
         this.day = Integer.parseInt(parseDate[2]);
     }
+
 
     @Override
     public String toString() {
@@ -56,15 +59,26 @@ public class MyDate implements Comparable<MyDate> {
         return this.day - anotherDate.day;
     }
 
+    public int compareTo1(MyDate anotherDate) {
+        if (this.year != anotherDate.year) {
+            return this.year - anotherDate.year;
+        } else if (this.month != anotherDate.month) {
+            return this.month - anotherDate.month;
+        } else {
+            return this.day - anotherDate.day;
+        }
+
+    }
+
     public long getDifferenceBetweenDays(MyDate date) {
-        LocalDate thisDate = LocalDate.of(this.year,this.month,this.day);
-        LocalDate dateToCompareWith = LocalDate.of(date.year,date.month,date.day);
+        LocalDate thisDate = LocalDate.of(this.year, this.month, this.day);
+        LocalDate dateToCompareWith = LocalDate.of(date.year, date.month, date.day);
         return ChronoUnit.DAYS.between(thisDate, dateToCompareWith);
     }
 
-    public static long getDifferenceBetweenDays(MyDate date1,MyDate date2) {
-        LocalDate first = LocalDate.of(date1.year,date1.month,date1.day);
-        LocalDate second = LocalDate.of(date2.year,date2.month,date2.day);
+    public static long getDifferenceBetweenDays(MyDate date1, MyDate date2) {
+        LocalDate first = LocalDate.of(date1.year, date1.month, date1.day);
+        LocalDate second = LocalDate.of(date2.year, date2.month, date2.day);
         return ChronoUnit.DAYS.between(first, second);
     }
 }
